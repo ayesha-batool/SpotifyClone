a58d4ffa9148a14db8d7d172d7684d1234f05a98
ayesha-batool <ayesha.dorara@gmail.com>
2025-09-25 20:57:09 +0500

    Updated something

--- SpotifyAdmin/src/App.jsx
+++ SpotifyAdmin/src/App.jsx
@@ -8,7 +8,7 @@ import ListAlbum from './pages/ListAlbum'
 import { Sidebar } from './components/Sidebar'
 import { Route,Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
-export const url=process.env.VITE_API_URL
+export const url=import.meta.env.VITE_API_URL || 'http://localhost:4000'
 const App = () => {
   return (
     <div className='flex items-center min-h-screen '>
